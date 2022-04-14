a4bb4a962bb800fe1bee02a8ca2901f4d8066fd0
VitorSantosH <wytorh@gmail.com>
2022-04-13 21:41:00 -0300

    header e footer

--- src/components/home/footer/Footer.jsx
+++ src/components/home/footer/Footer.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 
 import './Footer.css';
+import logoFeed from '../../../assets/iconFeed.png';
+import iconUser from '../../../assets/iconUser.png';
+import iconDiamant from '../../../assets/diamante.png';
+
 
 
 const Footer = () => {
 
     return (
         <div className="footer">
-            Footer
+            <div className="iconFeed">
+                <img src={logoFeed} alt="" />
+                <span>FEED</span>
+            </div>
+
+            <div className="iconPremiun">
+                <img src={iconDiamant} alt="" />
+                <span className="premiun">
+                    PREMIUN
+                </span>
+            </div>
+
+            <div className="iconPerfil">
+                <img src={iconUser} alt="" />
+                <span className="user">
+                    PERFIL
+                </span>
+            </div>
+
         </div>
     )
 }
